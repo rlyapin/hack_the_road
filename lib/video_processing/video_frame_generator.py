a4d9d609c8b3cdfacba35dac1b641966514012bc
Roman Lyapin <lyapin.rk@gmail.com>
2017-09-17 15:45:00 +0200

    Added the random video frame generator

--- lib/video_processing/video_frame_generator.py
+++ lib/video_processing/video_frame_generator.py
@@ -3,6 +3,7 @@
 import os
 import cv2
 import glob
+import random
 import numpy as np
 
 IMAGE_SHAPE = [224, 224, 3]
@@ -15,7 +16,7 @@ def single_frame_generator():
             n_frames = int(video_reader.get(7))
             # 1 second in OpenCV VideoCapture method would take 25 frames (?)
             # Im fine sampling frames every 0.2 seconds
-            sampled_indices = range(0, n_frames - 1, 5)
+            sampled_indices = range(0, n_frames, 5)
             for index in sampled_indices:
                 # Lame syntax on OpenCV side: 1th parameter is pointer location in the video
                 video_reader.set(1, index)
@@ -32,7 +33,15 @@ def batch_frame_generator(batch_size):
             generated_frames[i, :, :, :] = next(frame_generator)
         yield generated_frames
 
-# Sample code showing how these generators should be used
-# gen = batch_frame_generator(batch_size=10)
-# while True:
-#     frames = next(gen)
+def random_frame_generator(batch_size):
+    all_files = glob.glob("../../data/video/*.mp4")
+    while True:
+        picked_file = random.choice(all_files)
+        video_reader = cv2.VideoCapture(picked_file)
+        # Lame syntax on OpenCV side: 7th parameter is the number of frames
+        n_frames = int(video_reader.get(7))
+        picked_frame = random.choice(range(n_frames))
+        # Lame syntax on OpenCV side: 1th parameter is pointer location in the video
+        video_reader.set(1, picked_frame)
+        _, random_frame = video_reader.read()  
+        yield cv2.resize(random_frame, tuple(IMAGE_SHAPE[:2]))      
