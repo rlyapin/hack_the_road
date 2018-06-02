e32d9c89ec971e1de77ca6315d9e818c5d31687c
Roman Lyapin <lyapin.rk@gmail.com>
2018-06-02 12:45:49 +0200

    Refactored VAE code

--- lib/autoencoder/train_autoencoder.py
+++ lib/autoencoder/train_autoencoder.py
@@ -8,7 +8,7 @@ sys.path.append("..")
 import tensorflow as tf
 import numpy as np
 import tqdm
-from lib.autoencoder.pca_autoencoder import PCAAutoencoder
+from lib.autoencoder.vae_autoencoder import VAEAutoencoder
 from lib.utils.comma_ai_generator import batch_frame_generator
 from lib.utils.tf_saver import TfSaver
 
@@ -16,13 +16,13 @@ from lib.utils.tf_saver import TfSaver
 # Training autoencoder on comma ai video feed
 tf.reset_default_graph()
 frame_generator = batch_frame_generator("../data/comma/train/camera/", batch_size=8, skip_frames=10)
-model = PCAAutoencoder(hidden_size=100, input_shape=[160, 320, 3])
+model = VAEAutoencoder(hidden_size=100, input_shape=[160, 320, 3])
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
     # Loading most recent progress if present
-    saver = TfSaver('../data/models/comma_pca_autoencoder_folder/')
+    saver = TfSaver('../data/models/comma_vae_autoencoder_folder/')
     saver.load_latest_checkpoint(sess)
 
     print "Loss before training: "
@@ -30,7 +30,7 @@ with tf.Session() as sess:
 
     print "Started training: "
     for i in tqdm.tqdm(range(10000)):
-        model.train_op(sess, next(frame_generator), lr=1e-4)
+        model.train_op(sess, next(frame_generator), lr=1e-4, dropout_share=0.25, latent_loss_weight=0.0)
 
         if i % 100 == 0:
             saver.save_checkpoint(sess)
--- lib/autoencoder/vae_autoencoder.py
+++ lib/autoencoder/vae_autoencoder.py
@@ -1,85 +1,154 @@
-# The script with the definition of frame autoencoder model
+
+# The code to implement a variational autoencoder
 
 import tensorflow as tf
 import numpy as np
-import cv2
 
-IMAGE_SHAPE = [224, 224, 3]
 
-class FrameAutoencoder:
-    # The class to implment variational frame autoencoder
+class VAEAutoencoder:
+    def __define_placeholders(self, input_shape):
+        """Setting up autoencoder variables"""
+        # None in shape stands for the batch size
+        self.input_frame = tf.placeholder(dtype=tf.float32, shape=[None] + input_shape)
+        self.learning_rate = tf.placeholder(tf.float32, shape=[])
+        self.latent_loss_weight = tf.placeholder(tf.float32, shape=[])
+        self.dropout_share = tf.placeholder(tf.float32, shape=[])
 
-    def __init__(self, session, learning_rate=0.001):
-        self.session = session
-        self.learning_rate = learning_rate
-        self.latent_loss_scaling = 0.001
 
-        # Getting a batch of frames to process
-        self.frame = tf.placeholder("float", shape=[None, 224, 224, 3])
+    @staticmethod
+    def __add_conv_block(input_layer, n_filters_1, n_filters_2, dropout_share):
+        """A helper function to add convolution blocks in encoder part of vae
 
-        # Adding a collection of convolutional layers to reduce dimensioanlity of compression layer
-        conv_1 = tf.layers.conv2d(self.frame, filters=16, kernel_size=[5, 5], 
-                                  strides = 1, padding="same", activation=tf.nn.relu)
-        pool_1 = tf.layers.max_pooling2d(conv_1, pool_size=2, strides=2)
+        Each block here would consist of two convolutional layers with dropout followed by max pooling
+        Note that I am using RELU activation for convilutional layers
+        This means on its own the block output is a bad fit for the bottleneck vae layer (cannot be negative)
 
-        conv_2 = tf.layers.conv2d(pool_1, filters=32, kernel_size=[5, 5], 
-                                  strides = 1, padding="same", activation=tf.nn.relu)
-        pool_2 = tf.layers.max_pooling2d(conv_2, pool_size=2, strides=2)
+        Args:
+            input_layer: a tf layer that is fed into convolution block with [None, None, None, None] shape
+            n_filters_1: a number of filters for the first convolution layer
+            n_filters_2: a number of filters for the second convolution layer
+            dropout_share: a parameter to feed to the dropout layer
+        Returns:
+            tf layer after convolutions
+        """ 
+        conv_1 = tf.layers.conv2d(input_layer, filters=n_filters_1, kernel_size=[3, 3], 
+                                  strides=1, padding="same", activation=tf.nn.relu)
+        drop_1 = tf.nn.dropout(conv_1, keep_prob=1-dropout_share)
+        conv_2 = tf.layers.conv2d(drop_1, filters=n_filters_2, kernel_size=[3, 3], 
+                                  strides=1, padding="same", activation=tf.nn.relu)
+        drop_2 = tf.nn.dropout(conv_2, keep_prob=1-dropout_share)
+        pool_1 = tf.layers.max_pooling2d(drop_2, pool_size=2, strides=2)
+        return pool_1
 
-        conv_3 = tf.layers.conv2d(pool_2, filters=64, kernel_size=[5, 5], 
-                                  strides = 1, padding="same", activation=tf.nn.relu)
-        pool_3 = tf.layers.max_pooling2d(conv_3, pool_size=2, strides=2)
 
-        encoder_flat_1 = tf.contrib.layers.flatten(pool_3)
-        encoder_flat_2 = tf.layers.dense(encoder_flat_1, 2000, activation=tf.nn.relu)
+    @staticmethod
+    def __undo_conv_block(input_layer, n_filters_1, n_filters_2, dropout_share):
+        """A helper function to add deconvolution blocks in decoder part of vae
 
-        # Determining parameters of the latent space (Denoted by z and assumed to be gaussian)
-        z_mean = tf.layers.dense(encoder_flat_2, 100)
-        z_log_sigma_sq = tf.layers.dense(encoder_flat, 100)
+        The function is supposed to be a counterpart to __add_conv_block and repeat actions there in reverse
+        Note that unpooling part simply doubles image shape so mismatch with encoder can occur for odd shapes
+        RELU activations allow the block output to be a recovered image (if it is originally in [0, 1] range)
 
-        eps = tf.random_normal(tf.shape(z_mean), 0, 1)
-        z = z_mean + eps * tf.sqrt(tf.exp(z_log_sigma_sq))
+        Args:
+            input_layer: a tf layer that is fed into deconvolution block with [None, None, None, None] shape
+            n_filters_1: a number of filters for the first deconvolution layer
+            n_filters_2: a number of filters for the second deconvolution layer
+            dropout_share: a parameter to feed to the dropout layer
+        Returns:
+            tf layer after deconvolutions
+        """ 
+        unpool_1 = tf.image.resize_images(input_layer, 
+                                          2 * np.array(input_layer.get_shape().as_list()[1:3]), 
+                                          method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+        deconv_1 = tf.layers.conv2d_transpose(unpool_1, filters=n_filters_1, kernel_size=[3, 3], 
+                                              strides=1, padding="same", activation=tf.nn.relu)
+        drop_1 = tf.nn.dropout(deconv_1, keep_prob=1-dropout_share)
+        deconv_2 = tf.layers.conv2d_transpose(drop_1, filters=n_filters_2, kernel_size=[3, 3], 
+                                              strides=1, padding="same", activation=tf.nn.relu)
+        drop_2 = tf.nn.dropout(deconv_2, keep_prob=1-dropout_share)    
+        return drop_2
 
-        # Separately specifying compression
-        self.compression = tf.contrib.layers.flatten(z_mean)
 
-        # 28 * 28 * 32 comes from the shape of final pool layer
-        decoder_flat_2 = tf.layers.dense(z, 2000, activation=tf.nn.relu)
-        decoder_flat_1 = tf.layers.dense(decoder_flat_2, 28 * 28 * 64, activation=tf.nn.relu)
+    def __encoder(self, hidden_size):
+        """Setting up the encoding part of autoencoder"""
+        # Adding several convolution blocks
+        self.conv_block_1 = VAEAutoencoder.__add_conv_block(self.input_frame, 16, 32, self.dropout_share)
+        self.conv_block_2 = VAEAutoencoder.__add_conv_block(self.conv_block_1, 32, 64, self.dropout_share)
+        self.conv_block_3 = VAEAutoencoder.__add_conv_block(self.conv_block_2, 64, 128, self.dropout_share)
+        self.conv_block_4 = VAEAutoencoder.__add_conv_block(self.conv_block_3, 128, 256, self.dropout_share)
+        self.conv_block_5 = VAEAutoencoder.__add_conv_block(self.conv_block_4, 256, 512, self.dropout_share)
 
-        # Adding a collection of deconvolutions to map the shape of initial input
-        # Reversing pooling layer is achieved via deconvolution with more filters (x4) and reshaping
-        depool_3 = tf.reshape(decoder_flat_1, tf.shape(pool_3))
-        deconv_3 = tf.layers.conv2d_transpose(depool_3, filters=128, kernel_size=[5, 5], 
-                                              strides = 1, padding="same", activation=tf.nn.relu)
+        # Flattening the results and defining a bottleneck layer "z"
+        # Note that at htis stage there are no nonlinearities anymore
+        self.encoder_flat = tf.contrib.layers.flatten(self.conv_block_5)
+        self.z_mean = tf.layers.dense(self.encoder_flat, hidden_size)
+        self.z_log_sigma_sq = tf.layers.dense(self.encoder_flat, hidden_size)
 
-        depool_2 = tf.reshape(deconv_3, tf.shape(pool_2))
-        deconv_2 = tf.layers.conv2d_transpose(depool_2, filters=64, kernel_size=[5, 5], 
-                                              strides = 1, padding="same", activation=tf.nn.relu)
+        # Sampling the bottleneck given its mean and variance
+        self.eps = tf.random_normal(tf.shape(self.z_mean), 0, 1)
+        self.z = self.z_mean + self.eps * tf.sqrt(tf.exp(self.z_log_sigma_sq))
 
-        depool_1 = tf.reshape(deconv_2, tf.shape(pool_1))
-        deconv_1 = tf.layers.conv2d_transpose(depool_1, filters=12, kernel_size=[5, 5], 
-                                              strides = 1, padding="same")
 
-        # Restoring the original batch of frames
-        self.restored_frame = tf.reshape(deconv_1, tf.shape(self.frame))
+    def __decoder(self, input_shape):
+        """Setting up the decoding part of autoencoder"""
+        # Reversing encoder part until convolution blocks
+        # Using the shapes of corresponding encoder blocks to get at post convolution layer
+        self.decoder_flat = tf.layers.dense(self.z, self.encoder_flat.shape[1], activation=tf.nn.relu)
+        self.deconv_block_5 = tf.reshape(self.decoder_flat, tf.shape(self.conv_block_5))
 
-        # Picking the l2 loss beteen original and restored frames
-        self.l2_loss = 0.5 * tf.reduce_mean(tf.pow(self.frame - self.restored_frame, 2))
-        self.latent_loss = -0.5 * tf.reduce_sum(1 + z_log_sigma_sq - tf.square(z_mean) - tf.exp(z_log_sigma_sq))
-        self.vae_loss = tf.minimum(self.l2_loss + self.latent_loss_scaling * self.latent_loss, 10**8)
-        self.optimizer = tf.train.AdamOptimizer(self.learning_rate).minimize(self.vae_loss)
+        # Applying deconvolution layers
+        # Final restored frame is also deconvolution output
+        # So one must be careful when changing __undo_conv_block (especially activations)
+        self.deconv_block_4 = VAEAutoencoder.__undo_conv_block(self.deconv_block_5, 256, 256, self.dropout_share)
+        self.deconv_block_3 = VAEAutoencoder.__undo_conv_block(self.deconv_block_4, 128, 128, self.dropout_share)
+        self.deconv_block_2 = VAEAutoencoder.__undo_conv_block(self.deconv_block_3, 64, 64, self.dropout_share)
+        self.deconv_block_1 = VAEAutoencoder.__undo_conv_block(self.deconv_block_2, 32, 32, self.dropout_share)
+        self.restored_frame = VAEAutoencoder.__undo_conv_block(self.deconv_block_1, 16, 3, self.dropout_share)
 
-        self.session.run(tf.global_variables_initializer())
 
-    def compress(self, frames):
-        return self.session.run(self.compression, feed_dict={self.frame: frames})
+    def __loss(self):
+        """Defining loss used in training"""
+        self.l2_loss = 0.5 * tf.reduce_mean(tf.pow(self.input_frame - self.restored_frame, 2))
+        self.latent_loss = -0.5 * tf.reduce_sum(1 + self.z_log_sigma_sq  
+                                                - tf.square(self.z_mean) - tf.exp(self.z_log_sigma_sq))
+        self.loss = tf.minimum(self.l2_loss + self.latent_loss_weight * self.latent_loss, 10**8)
+ 
 
-    def restore(self, frames):
-        return self.session.run(self.restored_frame, feed_dict={self.frame: frames})
+    def __optimizer(self):
+        """Defining the optimizatin routine"""
+        self.optimizer = tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
 
-    def show_loss(self, frames):
-        return self.session.run(self.vae_loss, feed_dict={self.frame: frames})
 
-    def train_op(self, frames):
-        self.session.run(self.optimizer, feed_dict={self.frame: frames})
+    def __init__(self, hidden_size, input_shape):
+        """Combining everything together and initializing PCA autoencoder"""
+        self.__define_placeholders(input_shape)
+        self.__encoder(hidden_size)
+        self.__decoder(input_shape)
+        self.__loss()
+        self.__optimizer()
+
+
+    def compress(self, session, frames):
+        """Return an intermediary hidden layer"""
+        return session.run(self.z, feed_dict={self.input_frame: frames})
+
+
+    def restore(self, session, frames):
+        """Return frames pushed through autoencoder"""
+        return session.run(self.restored_frame, feed_dict={self.input_frame: frames})
+
+
+    def show_loss(self, session, frames, dropout_share=0.0, latent_loss_weight=0.0):
+        """Return reconstruction loss for given frames"""
+        return session.run(self.loss, feed_dict={self.input_frame: frames,
+                                                 self.dropout_share: dropout_share,
+                                                 self.latent_loss_weight: latent_loss_weight})
+
+
+    def train_op(self, session, frames, lr, dropout_share=0.0, latent_loss_weight=0.0):
+        """Perform one gradient step"""
+        session.run(self.optimizer, feed_dict={self.input_frame: frames, 
+                                               self.learning_rate: lr,
+                                               self.dropout_share: dropout_share,
+                                               self.latent_loss_weight: latent_loss_weight})
+
